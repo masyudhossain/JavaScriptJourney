a2d76c91fc1111f2a6b262a421f68da59d720836
Masyud Hossain <masyud.hossain2001@gmail.com>
2025-08-15 14:21:46 +0530

    add git ignore and dataTypes

---
+++ dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all newer code as newer version
+
+// alert(3+3)
+
+console.log(3 + 3)
+
+let fullName = "Hossain"
+let age = 25
+let mailId = "ho2001ain@gmail.com"
+
+console.table([fullName, age, mailId])
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+
+console.log(typeof undefined)
+console.log(typeof null);
