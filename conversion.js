17906dd7ee4f56fa2e9e41f1cdfcbd63b30337c0
Masyud Hossain <masyud.hossain2001@gmail.com>
2025-08-15 14:21:26 +0530

    Conversion of data types

---
+++ conversion.js
@@ -0,0 +1,25 @@
+let score = null
+let score1 = "99mas"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+let valueInNumber1 = Number(score1)
+console.log(typeof valueInNumber);
+console.log(typeof valueInNumber1);
+console.log(valueInNumber)
+console.log(valueInNumber1)
+
+//"33" => 33
+// "99mas" = NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = 0
+let isLoggedIn1 = 22
+let isLoggedIn2 = "masyud"
+let booleanIsLoggedIn = Boolean(isLoggedIn) // false
+let booleanIsLoggedIn1 = Boolean(isLoggedIn1) //  true
+let booleanIsLoggedIn2 = Boolean(isLoggedIn2) // true
+console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn1);
+console.log(booleanIsLoggedIn2);
