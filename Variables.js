f5f4dfde22d1a799368b9cf2db2ecaa438375cbe
Masyud Hossain <masyud.hossain2001@gmail.com>
2025-08-15 13:48:01 +0530

    add file Test.js and Variable.ja

---
+++ Variables.js
@@ -0,0 +1,11 @@
+const customerId = 111
+let customerName = "Masyud Hossain"
+let customerEmail = "masyud@gmail.com"
+var customerPassword = "123546"
+let customeState
+customerCity = "Kolkata"
+
+// console.log(customerId)
+// console.log(customerCity)
+
+console.table([customerId,customerName,customerEmail,customerCity,customeState])
